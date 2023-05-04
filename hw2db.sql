886527328b0c5752ed6e9fbe14248e142a87c121
Mahdiyeh <www.mahdiyeh.mazani@gmail.com>
2023-05-04 11:52:28 +0330

    Add files via upload

---
+++ hw2db.sql
@@ -0,0 +1,35 @@
+-- 1
+-- SELECT first_name, last_name from staff;
+--2
+-- select customer_id , last_name , first_name from customer
+-- order by last_name
+--3
+-- SELECT city_id,city from city
+-- where country_id=60;
+--4
+-- SELECT * FROM customer
+-- WHERE first_name LIKE 'J%' and store_id=1;
+--5
+-- SELECT  *  FROM film 
+-- where rental_rate=(select max(rental_rate) from film)
+-- limit 5;
+--6
+-- SELECT DISTINCT amount FROM payment;
+--7
+-- SELECT count( customer_id )  from payment
+-- WHERE  amount IN (0.99,4.99,0.00);
+--8
+
+--9
+
+--*10
+-- select count(rating) from film 
+-- where rating=(select distinct rating from film);
+--امتیازی
+--*1
+-- select first_name,last_name from customer , all from payment
+-- where customer_id=344 and payment_date between #2007-02-15
+-- and #2007-02-20;
+--2
+-- SELECT first_name || ' ' || last_name as full_name FROM customer;
+--3
